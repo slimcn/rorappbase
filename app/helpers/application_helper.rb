f71f169b0ec56fdbabd7c2a73b5b5bf5deacbb94
shangjm <sjmtmee@gmail.com>
2010-07-28 09:34:48 +0800

    2dc_jqgrid: include jquery and jqgird.But this grid has a bug:if the column just one,the data would unfinished.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,39 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def buttonhtml(buttons)
+    html = ""
+    @sheet_options = { } if not @sheet_options.is_a?(Hash)
+    @sheet_options = { "edit_form_type" => "jqgrid_form" }.merge(@sheet_options)
+    edit_form_type = @sheet_options.has_key?("edit_form_type") ? @sheet_options["edit_form_type"] : "jqgrid_form"
+    buttons.each do |btn|
+      html += "<button id='btn_" + btn[0] +"' class='rest-button #{edit_form_type}' pre_url='/#{controller_name}' >" + btn[1] + "</button>"
+    end
+    return html
+  end
+
+  # 调用jqgrid
+  def jqgrid_index(fields, *args)
+    html = ""
+
+    # 获得基本变量
+    controller_name = controller.controller_name
+    name_single = controller_name.downcase.singularize
+    class_name_cn = I18n.t("activerecord.models.#{name_single}")
+    nav_end = { :add => true, :edit => true, :delete => true, :edit_url => "/#{controller_name}/post_data", :error_handler => "afterSubmit", :height => 450, :rows_per_page => 20, :selection_handler => "handleSelection"}
+    #nav_end = { :add => true, :edit => true, :delete => true, :edit_url => "/#{controller_name}/post_data", :error_handler => "afterSubmit", :height => 450, :rows_per_page => 20, :selection_handler => "handleSelection", :inline_edit => true}
+
+    # 若fields中无label，则补上
+    fields.each_index do |index|
+      fields[index] = { :label => I18n.t("activerecord.attributes.#{name_single}.#{fields[index][:field]}")}.merge(fields[index])
+    end
+
+    # fields数组 若第1、2个定义非id，则补上id字段
+    if (not (fields[0][:field] == "id")) and (fields.size>1 and not (fields[1][:field] == "id"))
+      fields.insert(0,{ :field => "id", :label => "ID", :width => 35, :resizable => false })
+    end
+
+    html += jqgrid(class_name_cn, "tableRecs", "/#{controller_name}", fields, nav_end)
+
+    return html
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,97 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$j = jQuery.noConflict(); // $j代替jQuery的$,防止jQuery与Prototype共用的冲突
+
+$j(document).ready(function(){
+    var responseData;
+
+    $j('.expanded').addClass('collapsed');
+    $j('.expanded').removeClass('expanded');
+
+    $j('#div-nav-v li').click(function() {
+        // 设置选中状态
+        $j('.selected').removeClass('selected');
+        $j(this).children('a').addClass('selected');
+        // 设置子节点是否展开
+        var tmpid = this.id;
+        $j('#ul'+tmpid).attr('class',($j('#ul'+tmpid).attr('class'))=='expanded'?'collapsed':'expanded');
+        // todo:若为叶子节点，则设置标题
+        if ($j(this).attr('class') == 'child') {
+            $j('head > title').attr('text',$j('.div-nav-subs > div > h2').attr('text'));
+        }
+
+        return false;
+    });
+
+<!-- // start // operate button action -->
+    $j("button").click(function(){
+        var btn_type = $j(this).attr("id");
+        var pre_url = $j(this).attr("pre_url");
+        //TODO: 若按钮类不属 jqgrid_form 调用普通界面  若属jqgrid_form，调用jqgrid默认界面
+        if ($j(this).hasClass("jqgrid_form")) {
+            if (btn_type=="btn_add") {
+                $j("#add_tableRecs").click();
+            } else if (btn_type=="btn_search") {
+                $j("#tableRecs")[0].toggleToolbar();
+                    //$j("#search_tableRecs").click();
+            } else if (btn_type=="btn_refresh") {
+                $j("#refresh_tableRecs").click();
+            } else { // need id when operate
+                $j("#tableRecs_select_button").click(); // 设置记录id
+                if (var_id > 0) {
+                    if (btn_type=="btn_update") {
+                        $j("#edit_tableRecs").click();
+                    } else if (btn_type=="btn_delete") {
+                        $j("#del_tableRecs").click();
+                    } else if (btn_type=="btn_show") {
+                        $j("#show_tableRecs").click();
+                    } else {
+                        alert("未知操作请求！");
+                    }
+                } else {
+                    alert("未知操作请求！");
+                }
+            }
+        } else if ($j(this).hasClass("multi_model")) {
+            if (btn_type=="btn_add") {
+                openwin(pre_url+'/new');
+            } else if (btn_type=="btn_search") {
+                $j("#tableRecs")[0].toggleToolbar();
+            } else if (btn_type=="btn_refresh") {
+                $j("#refresh_tableRecs").click();
+            } else { // need id when operate
+                $j("#tableRecs_select_button").click(); // 设置记录id
+                if (var_id > 0) {
+                    if (btn_type=="btn_update") {
+                        openwin(pre_url+'/'+var_id+'/edit')
+                    } else if (btn_type=="btn_delete") {
+                        $j("#del_tableRecs").click();
+                    } else if (btn_type=="btn_show") {
+                        openwin(pre_url+'/'+var_id)
+                    } else {
+                        alert("未知操作请求！");
+                    }
+                } else {
+                    alert("未知操作请求！");
+                }
+            }
+
+        }
+    });
+<!-- // end // operate button action -->
+
+
+    function openwin(url){
+        window.open(url,'_self');
+    }
+});
+
+
+function ExpandMenu(emcode, parentcode){
+    var objParent = document.getElementById(parentcode);
+    objParent.className = (objParent.className.toLowerCase() == "expanded"?"expanded":"expanded");
+    var obj = document.getElementById(emcode);
+    obj.className = (obj.className.toLowerCase() == "expanded"?"expanded":"expanded");
+}
+
